c6c2dead451754157f57a2c0cbd9c3be0c475a5d
nakanaka773 <hirayama773@gmail.com>
2023-09-23 07:37:22 +0900

    googleanaliticsを足したよん

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
   return (
     <Html lang="ja">
-      <Head />
+          <Head>
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}');
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
       <body>
         <Main />
         <NextScript />
